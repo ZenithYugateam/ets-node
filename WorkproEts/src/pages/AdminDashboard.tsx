1d3d8691503e51f57a1a3e0aa1a1813209b1ef48
pradeep211221 <122143408+pradeep211221@users.noreply.github.com>
2024-12-24 13:24:40 +0530

    Revert "View tasks"

--- WorkproEts/src/pages/AdminDashboard.tsx
+++ WorkproEts/src/pages/AdminDashboard.tsx
@@ -6,8 +6,7 @@ import ProjectManagement from '../components/admin/ProjectManagement';
 import LeaveApprovals from '../components/shared/LeaveApprovals';
 import { fetchDashboardStats, fetchUsers, fetchDepartments } from '../api/admin';
 import { toast } from 'react-toastify';
-import { TaskStepperDisplay } from '../components/admin/TaskStepperDisplay';
-import TasksDisplay from '../components/admin/TasksDisplay';
+
 interface User {
   _id: string;
   name: string;
@@ -160,7 +159,6 @@ const AdminDashboard = () => {
       {/* Admin components */}
       <UserManagement adminId={localStorage.getItem('userId')} />
       <ProjectManagement />
-      <TasksDisplay />
     </div>
   );
 };
